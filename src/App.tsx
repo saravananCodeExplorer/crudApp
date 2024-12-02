30198c540fb96b21923b2deda0d704a4c7219959
saravananCodeExplorer <evergoldhariny2007@gmail.com>
2024-12-02 16:57:10 +0530

    fetch the data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useEffect ,useState} from 'react';
+import axios from 'axios';
 
 function App() {
+  useEffect(()=>{
+    fetchData();
+
+  },[])
+
+  const fetchData = async()=>{
+    const response = await axios.get("http://localhost:8080/Employee")
+    console.log(response.data)
+  }
   return (
     <div className="App">
    <h1>header</h1>
+
   </div>
   );
 }
